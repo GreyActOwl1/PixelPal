aaa418946afb4dc568dd1efbe7809411189c77ca
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-05-06 15:34:28 -0400

    Extract sign up from validation

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,7 +1,7 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,38 +10,35 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { signUpValidationSchema } from "@/lib/validation";
 
-
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
-
-const SignUpForms = () =>  {
+const SignUpForms = () => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof signUpValidationSchema>>({
+    resolver: zodResolver(signUpValidationSchema),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
+
     },
-  })
- 
+  });
+
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof signUpValidationSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
-
-
-
 
   return (
     <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/public/assets/images/logo.svg" alt="PixelPal Logo"/>
+      </div>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
@@ -50,7 +47,7 @@ const SignUpForms = () =>  {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="user1" autoComplete="username"{...field} />
+                <Input placeholder="user1" autoComplete="username" {...field} />
               </FormControl>
               <FormDescription>
                 This is your public display name.
@@ -59,10 +56,10 @@ const SignUpForms = () =>  {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" className="shad-button_primary">Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
 
 export default SignUpForms;
