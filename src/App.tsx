b354e32976f5b7d26eda97014e6d4d0cb0df36e3
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-05-06 08:36:26 -0400

    Add blank pages & paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import RootLayout from "./_root/RootLayout";
+import AuthLayout from "./_auth/AuthLayout";
+import { Home } from "./_root/pages";
 import "./globals.css";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>AuthLayout
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   );
 };
 
