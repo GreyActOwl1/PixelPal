56940f5633081b7595bc5d5ba9aa4aac9a3d4d6d
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-05-08 06:30:10 -0400

    Add database configs

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,9 +1,16 @@
 import { Client, Account, Databases, Storage, Avatars } from 'appwrite';
+import { DatabaseIcon } from 'lucide-react';
 
 //TODO: move .env.local to doppler
 export const appwriteConfig = {
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    endpoint: import.meta.env.VITE_APPWRITE_API_ENDPOINT
+    endpoint: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,    
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    usersCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+    postsCollectionId: import.meta.env.VITE_APPWRITE_POSTS_COLLECTION_ID,
+    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+
 };
 
 export const appwriteClient = new Client();
